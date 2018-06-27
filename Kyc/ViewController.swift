50e5bd66c4f62d6c0da0faa3b301b7554e742b56
Bkmsx <tien@novum.capital>
2018-06-27 14:44:27 +0700

    Initial Commit

---
+++ Kyc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kyc
+//
+//  Created by Lai Trung Tien on 6/27/18.
+//  Copyright © 2018 Lai Trung Tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
